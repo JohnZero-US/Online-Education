f87ac27fc977a2169330b3d64ddd8818d7e0c142
刘奇隆 <1134250142@qq.com>
2019-05-03 06:28:42 +0800

    2019 年 5 月 3 日, 6 点 28 分

--- view/src/main/java/com/johnzero/view/ViewApplication.java
+++ view/src/main/java/com/johnzero/view/ViewApplication.java
@@ -2,8 +2,10 @@ package com.johnzero.view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ViewApplication {
 
     public static void main(String[] args) {
--- view/src/main/java/com/johnzero/view/controller/HomeController.java
+++ view/src/main/java/com/johnzero/view/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.johnzero.view.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -16,9 +19,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Value("${server.port}")
+    String port;
 
-    @GetMapping("home")
-    public String home() {
+    @GetMapping(value = {"/", "/home"})
+    public String home(Model model) {
+        model.addAttribute("port", port);
         return "home";
     }
 }
