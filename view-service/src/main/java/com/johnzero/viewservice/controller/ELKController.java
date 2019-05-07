5deea34bcb5d451cb59daa970f57b82d1322e2fa
刘奇隆 <1134250142@qq.com>
2019-05-07 17:07:55 +0800

    提交

---
+++ view-service/src/main/java/com/johnzero/viewservice/controller/ELKController.java
@@ -0,0 +1,14 @@
+package com.johnzero.viewservice.controller;
+
+/**
+ * 描述:
+ */
+/*
+  Created by IntelliJ IDEA.
+  Type: Class
+  User: John Zero
+  DateTime: 2019/5/7 17:03
+  Description: 
+*/
+public class ELKController {
+}
